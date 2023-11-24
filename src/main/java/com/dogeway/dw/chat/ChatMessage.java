69a058799009044ba0f83a75de53392beaeca9bb
MDAMIAN <soner.12d@gmail.com>
2023-11-23 23:39:15 -0600

    Agregando package chat

--- src/main/java/com/dogeway/dw/chat/ChatMessage.java
+++ src/main/java/com/dogeway/dw/chat/ChatMessage.java
@@ -1,22 +1,21 @@
 package com.dogeway.dw.chat;
-import lombok.Data;
-import javax.persistence.*;
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.io.Serializable;
 
 @Entity
 @Data
 @Table(name="messages")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatMessage implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String user;
+    private Long sendTo;
     private String message;
-
-
-
-    // Getters y Setters
-
-    // Constructor por defecto y constructor con parámetros si es necesario
 }
--- src/main/java/com/dogeway/dw/chat/ChatMessageRepository.java
+++ src/main/java/com/dogeway/dw/chat/ChatMessageRepository.java
@@ -1,8 +1,6 @@
 package com.dogeway.dw.chat;
-import com.chat.demo.model.ChatMessage;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
